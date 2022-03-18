5e853ecea298b167feb90379ac913bb69eb1de7a
Miguel Cuadrado <miguel.cuadrado@iothinktechnologies.co>
2022-03-18 10:12:18 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AccessComponent } from './components/user/access/access.component';
+import { RecoveryComponent } from './components/user/recovery/recovery.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccessComponent,
+    RecoveryComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
